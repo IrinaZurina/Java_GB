8b142f96b5656ed13f6a3d9ff487e670414154ce
IrinaZurina <zurinai@yandex.ru>
2023-08-03 19:26:56 +0300

    Exceptions Seminar 2 homework added

--- Seminars/Exceptions/src/main/java/org/example/Main.java
+++ Seminars/Exceptions/src/main/java/org/example/Main.java
@@ -1,5 +1,53 @@
-package org.example;public class Main {
+package org.example;
+
+
+import java.util.Scanner;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(getFloatNumber());
+
+        String text = readLine();
+        System.out.println(text);
+
+    }
+
+    /*
+    ** Реализуйте метод, который запрашивает у пользователя ввод дробного числа (типа float), и возвращает
+    * введенное значение. Ввод текста вместо числа не должно приводить к падению приложения, вместо этого,
+    * необходимо повторно запросить у пользователя ввод данных.
+     */
+    public static float getFloatNumber() {
+        System.out.print("Введите десятичную дробь: ");
+        Scanner scanner = new Scanner(System.in);
+        String userInput = scanner.nextLine();
+
+        float res = 0;
+        if (isValid(userInput)) res = Float.parseFloat(userInput);
+        else {
+            System.out.println("Неправильно введено число!");
+            res = getFloatNumber();
+        }
+        return res;
+    }
+
+    public static boolean isValid(String testString){
+        try{
+            Float.parseFloat(testString);
+            return true;
+        } catch (NumberFormatException e){
+            return false;
+        }
+    }
+
+    /*Разработайте программу, которая выбросит Exception, когда пользователь вводит пустую строку.
+    Пользователю должно показаться сообщение, что пустые строки вводить нельзя.*/
+    public static String readLine(){
+        System.out.print("Введите текст: ");
+        Scanner scanner = new Scanner(System.in);
+        String line = scanner.nextLine();
+        if (line.length() == 0)
+            throw  new RuntimeException("Нельзя вводить пустую строку!");
+        return line;
     }
 }
